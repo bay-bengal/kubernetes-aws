cc60d1483f9a27372d7b1136cb6bb8a28a170645
bay-bengal <jahidur.rahman@zmartagroup.com>
2022-10-09 21:55:28 +0200

    added provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+# Configure the AWS Provider
+provider "aws" {
+  profile = "terraform"
+  region  = "eu-west-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
